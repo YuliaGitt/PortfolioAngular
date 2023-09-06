3c4eface2557b012b959df5f417800ca2950ca74
Yulia <yuliagub07@gmail.com>
2023-09-06 16:02:23 +0300

    New components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    ShopComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule
